0d9037eb06b8e850a71b3d7db71bab8e2848a8f5
Maxime BERGEON <webmastercr3a@gmail.com>
2015-03-18 10:14:22 +0100

    Some changed to the Quizz module : now resume the curren video. Still looking for intercepting timeupdate and play events

--- src/widgets/Quizz.js
+++ src/widgets/Quizz.js
@@ -11,7 +11,9 @@ IriSP.Widgets.Quizz.prototype.defaults = {
     // container: "imageContainer"
 }
 
-IriSP.Widgets.Quizz.prototype.template = '<div class="Ldt-Quizz-Container"><h1 class="Ldt-Quizz-Title">{{question}}</h1>'
+IriSP.Widgets.Quizz.prototype.template = '<div class="Ldt-Quizz-Container">'
+										+ '<div class="Ldt-Quizz-Score"></div>'
+										+ '<h1 class="Ldt-Quizz-Title">{{question}}</h1>'
 										+ '	<fieldset class="Ldt-Quizz-Questions">'
 										+ '	</fieldset>'
 										+ '	<div class="Ldt-Quizz-Submit">'
@@ -32,14 +34,12 @@ IriSP.Widgets.Quizz.prototype.annotationTemplate = '';
 
 IriSP.Widgets.Quizz.prototype.update = function(annotation) {
 
-    $(".Ldt-ImageDisplay-Overlay-Left").on("click", function (event) { event.preventDefault(); });
-    $(".Ldt-ImageDisplay-Overlay-Right").on("click", function (event) { event.preventDefault(); });
-
 	//Pause the current video
 	this.media.pause();
 
 	this.annotation = annotation;
 
+	var _this = this;
 	var question = annotation.content.data.question;
 	var answers = annotation.content.data.answers;
 
@@ -47,7 +47,7 @@ IriSP.Widgets.Quizz.prototype.update = function(annotation) {
 	$(".Ldt-Ressources-Overlay").hide();
 	$(".Ldt-Quizz-Votes").hide();
 
-	$(".Ldt-Quizz-Container .Ldt-Quizz-Title").html(question);
+	$(".Ldt-Quizz-Container .Ldt-Quizz-Title").html("Question "+ (annotation.number+1) + "/" + this.totalAmount +" : " +question);
 
 	var i = 0
 
@@ -84,12 +84,29 @@ IriSP.Widgets.Quizz.prototype.update = function(annotation) {
 
 	//In case we click on the first "Skip" link
 	$(".quizz-submit-skip-link").click({media: this.media}, function(event) {
-		$(".Ldt-Quizz-Votes").fadeOut();
-		$(".Ldt-Quizz-Overlay").hide();
-		$(".Ldt-Ressources-Overlay").hide();
+		_this.hide();
 		event.data.media.play();
 	});
+
+	var _this = this;
+	//Trying to catch timeupdate and play events
+    this.onMediaEvent("timeupdate", function(_time) {
+        if (_time != annotation.begin) {
+			console.log("timeupdate " + _time);
+		}
+    });
+    this.onMediaEvent("play", function(_time) {
+        if (_time != annotation.begin) {
+			console.log("play " + _time);
+		}
+    });
 };
+
+IriSP.Widgets.Quizz.prototype.hide = function() {
+	$(".Ldt-Quizz-Votes").fadeOut();
+	$(".Ldt-Quizz-Overlay").hide();
+	$(".Ldt-Ressources-Overlay").hide();
+}
 
 IriSP.Widgets.Quizz.prototype.answer = function() { 
 	//alert(this.annotation.content.data.question);
@@ -102,6 +119,7 @@ IriSP.Widgets.Quizz.prototype.answer = function() {
 	var answers = this.annotation.content.data.answers;
 	var faux = false;
 	var i =0;
+	var _this = this;
 
 	while (i < answers.length && faux == false) {
 		console.log("Question : "+ i +" => réponse : "+ $(".quizz-question-" + i).is(':checked'));
@@ -111,15 +129,22 @@ IriSP.Widgets.Quizz.prototype.answer = function() {
 		i++;
 	}
 
+	$(".Ldt-Quizz-Score").fadeIn();
+
 	//Todo : display the result in a cool way :)
 	if (faux == true) {
 		alert("Mauvaise réponse");
+		this.correct[this.annotation.number] = 0;
 	}
 	else
 	{
 		alert("Bonne réponse !");
+		this.correct[this.annotation.number] = 1;
 	}
 
+	var correctness = this.globalScore();
+	$(".Ldt-Quizz-Score").html(correctness[0] + " bonne(s) réponse(s) / " + correctness[1] + " mauvaise(s) réponse(s)");
+	
 	//Hide the "Validate" button and display the UI dedicated to votes
 	$(".Ldt-Quizz-Submit").fadeOut();
 	$(".Ldt-Quizz-Votes").delay(500).fadeIn();
@@ -130,8 +155,7 @@ IriSP.Widgets.Quizz.prototype.answer = function() {
 		//Resume the current video
 		event.data.media.play();
 
-		$(".Ldt-Quizz-Votes").fadeOut();
-		$(".Ldt-Quizz-Overlay").hide();
+		_this.hide();
 		$(".Ldt-Ressources-Overlay").hide();
 
 	});
@@ -143,6 +167,23 @@ IriSP.Widgets.Quizz.prototype.skip_question = function() {
 
 }
 
+IriSP.Widgets.Quizz.prototype.globalScore = function() { 
+	//Define 2 variables to know how good and bad answers there are
+	var ok = 0;
+	var ko = 0;
+	for(var i = 0; i < this.totalAmount; i++) {
+		if (this.correct[i] == 1) {
+			ok++;
+		}
+		else if (this.correct[i] == 0)
+		{
+			ko++;
+		}
+	}
+	var array = [ok, ko];
+	return array;
+}
+
 IriSP.Widgets.Quizz.prototype.draw = function() {   
 
     var _annotations = this.getWidgetAnnotations().sortBy(function(_annotation) {
@@ -152,10 +193,9 @@ IriSP.Widgets.Quizz.prototype.draw = function() {
 	
 	_this.container = $("<div class='Ldt-Quizz-Overlay right_panel'></div>").prependTo($("[widget-type*=Player]"));
 	_this.ressourcesContainer = $("<div class='Ldt-Ressources-Overlay left_panel'></div>").prependTo($("[widget-type*=Player]"));
+	_this.container.html(this.template);
 
 	$(".Ldt-Quizz-Overlay").hide();
-
-	_this.container.html(this.template);
 
     console.log("Quizz was drawn");
 
@@ -163,7 +203,14 @@ IriSP.Widgets.Quizz.prototype.draw = function() {
 		_this.answer();
     });
 
+	_this.totalAmount = _annotations.length;
+	_this.number = 0;
+	_this.correct = [];
+
     _annotations.forEach(function(_a) {
+		//Fix each annotation as "non-answered yet"
+		_this.correct.push(-1);
+		_a.number = _this.number++;
         _a.on("enter", function() {
             _this.update(_a);
         });
